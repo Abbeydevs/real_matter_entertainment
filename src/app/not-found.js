e215d9de08f852605b985f430e19ac5e54835d9e
Abbeydevs <abbey.developer@gmail.com>
2023-08-21 00:59:16 +0100

    Initial commit

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div>
+      404 Error | This page is most likely not available on this website or
+      check if you misspelt the url.
+    </div>
+  );
+}
