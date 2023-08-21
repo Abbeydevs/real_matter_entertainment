fe45621d72ecab3be5b03ac18ce2c598cc976fb5
Obadara16 <obadararoqeeb@gmail.com>
2023-08-21 17:55:27 +0100

    artists -> component added

--- src/components/artists.jsx
+++ src/components/artists.jsx
@@ -1,11 +1,23 @@
+import React from 'react';
+import Image from 'next/image';
+import { artistData } from './data';
 
-
-import React from 'react'
-
-export default function artists() {
+export default function Artists() {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="container">
+      <div className="artist__container">
+        <div className="artist__header">
+          <h2 className="artist__heading">Artists</h2>
+          <button className="artist__bookArtistButton">Book Artist</button>
+        </div>
+        <div className="artist__imageGrid">
+          {artistData.map((artist) => (
+            <div className="artist__imageItem" key={artist.id}>
+              <Image src={artist.image} alt={artist.alt} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/data.js
@@ -0,0 +1,53 @@
+import explore1 from "../../public/explore1.svg"
+import explore2 from "../../public/explore2.svg"
+import explore3 from "../../public/explore3.svg"
+import artist1 from "../../public/artist1.svg"
+import artist2 from "../../public/artist2.svg"
+import artist3 from "../../public/artist3.svg"
+
+export const exploreData = [
+    {
+      id: 1,
+      image: explore1,
+      alt: "Explore 1",
+      artist: "Artist 1",
+      song: "Song 1",
+    },
+    {
+      id: 2,
+      image: explore2,
+      alt: "Explore 2",
+      artist: "Artist 2",
+      song: "Song 2",
+    },
+    {
+      id: 3,
+      image: explore3,
+      alt: "Explore 3",
+      artist: "Artist 3",
+      song: "Song 3",
+    },
+  ];
+
+
+
+export const artistData = [
+    {
+      id: 1,
+      image: artist1,
+      alt: "Artist 1",
+    },
+    {
+      id: 2,
+      image: artist2,
+      alt: "Artist 2",
+    },
+    {
+      id: 3,
+      image: artist3,
+      alt: "Artist 3",
+    },
+  ];
+  
+  
+  
