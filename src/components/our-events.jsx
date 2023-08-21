2472ffe5ba89fda932c96e9dae7d56a8fec8991e
Obadara16 <obadararoqeeb@gmail.com>
2023-08-21 17:59:00 +0100

    events => component added

--- src/components/our-events.jsx
+++ src/components/our-events.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function OurEvents() {
   return (
-    <div>our-events</div>
+    <section className="container">
+      <div className="events__container">
+        <div className="events__content">
+          <div className="events__background">
+          <div className="events__center">
+            <h2 className="events__heading">Our Events</h2>
+            <p className="events__subheading">Do you want to attend our events? Book now</p>
+            <button className="events__button">Book Now</button>
+          </div>
+          </div>
+          
+        </div>
+      </div>
+    </section>
   )
 }
