6ff0a6b44a748ed0fabbfe5bf4837be599116548
Obadara16 <obadararoqeeb@gmail.com>
2023-08-21 18:04:22 +0100

    explore -> component added

--- src/components/explore.jsx
+++ src/components/explore.jsx
@@ -1,7 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
+import { exploreData } from './data'
 
 export default function Explore() {
   return (
-    <div>explore</div>
+    <div className="explore__container">
+      <h2 className="explore__heading">Explore our songs</h2>
+      <div className="explore__grid">
+        {exploreData.map((item) => (
+          <div className="explore__card" key={item.id}>
+            <Image src={item.image} alt={item.alt} className="explore__image" width={100} height={100} />
+            <p className="explore__text"><span className='explore__artist'>{item.artist}</span> - {item.song}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
