c593d247d0c11fceef67f7397bd3bcd1502e3ad7
Obadara16 <obadararoqeeb@gmail.com>
2023-08-21 12:54:32 +0100

    navbar -> component created

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+"use client"
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import logo from "../../public/logo.svg"
 
-export default function Nav() {
+const MENU_ITEMS = [
+  { label: "Artists", path: "/artists" },
+  { label: "Releases", path: "/releases" },
+  { label: "Events", path: "/events" },
+  { label: "Contact", path: "/contact" },
+];
+
+const Nav = () => {
+  const [isNavActive, setIsNavActive] = useState(null);
+  const [activeItemIdx, setActiveItemIdx] = useState(-1);
+
+  const NavItem = ({ label, path, isActive }) => {
+    return (
+      <Link href={path}>
+        <span className={`nav__link ${isActive ? "active" : ""}`}>{label}</span>
+      </Link>
+    );
+  };
+
   return (
-    <div>nav</div>
-  )
-}
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"}>
+          <Image src={logo} alt="logo" width="400px" height="400px"/>
+        </Link>
+        <div
+          onClick={() => setIsNavActive(!isNavActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${isNavActive ? "active" : ""} nav__menu-list`}>
+          {MENU_ITEMS.map((item, idx) => (
+            <div
+              onClick={() => {
+                setActiveItemIdx(idx);
+                setIsNavActive(false);
+              }}
+              key={item.label}
+            >
+              <NavItem label={item.label} path={item.path} isActive={activeItemIdx === idx} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
