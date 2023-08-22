6673e33aadbcad0814a5806c9cfbee65fc4d4bfa
Obadara16 <obadararoqeeb@gmail.com>
2023-08-22 08:53:54 +0100

    footer -> component created

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import logo from "../../public/logo.svg"
+import Image from 'next/image';
+
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <div>footer</div>
+    <section className='container'>
+      <footer className="footer__container">
+      <div className="footer__logo">
+          <Image src={logo} alt="logo" width="fit" height="250"/>
+      </div>
+      <div className="copyright__text">© {currentYear} 2023 COPYRIGHT Real Matters Entertainment - All Rights Reserved </div>
+    </footer>
+    </section>
   )
 }
