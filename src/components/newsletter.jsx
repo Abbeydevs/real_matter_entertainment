7f8028223e787abfdae1b1b2c118e245b2392441
Obadara16 <obadararoqeeb@gmail.com>
2023-08-22 08:09:39 +0100

    newsletter -> component created

--- src/components/newsletter.jsx
+++ src/components/newsletter.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Newsletter() {
   return (
-    <div>newsletter</div>
+    <section className='container'>
+      <div className="newsletter-container">
+        <h1 className="newsletter-heading">Newsletter</h1>
+        <p className="newsletter-subheading">Grab our Monthly Newsletter and stay tuned</p>
+        <div className="input-container">
+          <input type="email" className="newsletter-input" placeholder="Your email address" />
+          <button className="newsletter-submit">Sign up</button>
+        </div>
+      </div>
+    </section>
   )
 }
